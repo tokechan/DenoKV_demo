f5651b479217f0634c19f9159140196383f1a8f4
tokechan <fleatoke@gmail.com>
2025-05-03 15:28:14 +0900

    Add:put delete API

--- deno-hono-api/main.ts
+++ deno-hono-api/main.ts
@@ -1,6 +1,12 @@
 import { Hono } from "hono";
 import { DOMParser } from "@b-fuze/deno-dom";
 
+interface Bookmark {
+  readonly url: string;
+  readonly title: string;
+}
+
+
 const app = new Hono();
 
 const kv = await Deno.openKv();
@@ -48,7 +54,35 @@ app.post("/api/bookmarks", async (c) => {
   return c.json({ result }, 201);
 });
 
+app.get("/api/bookmarks", async (c) => {
+  const bookmarkListIterator = kv.list<Bookmark>({ prefix: ["bookmark"] });
+  const bookmarks = [];
+  for await (const bookmark of bookmarkListIterator) {
+    bookmarks.push(bookmark);
+  }
+  const bookmarkReversed = bookmarks.toReversed();
+  return c.json({ bookmarks: bookmarkReversed });
+});
 
+
+app.put("/api/bookmarks/:url", async (c) => {
+  const url = c.req.param("url");
+  const decodedUrl = decodeURIComponent(url);
+  const body = await c.req.parseBody<{ title: string }>();
+  const titel  = body.title;
+  const result = await kv.set(["bookmark", decodedUrl], {
+    url: decodedUrl,
+    titel,
+  });
+  return c.json({ result });
+});
+
+app.delete("/api/bookmarks/:url", async (c) => {
+  const url = c.req.param("url");
+  const decodedUrl = decodeURIComponent(url);
+  const result = await kv.delete(["bookmark", decodedUrl]); 
+  return c.json({ result });
+});
 
 
 Deno.serve(app.fetch)
